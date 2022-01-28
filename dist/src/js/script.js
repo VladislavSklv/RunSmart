731d5f221d6261dfcf37fb38a9d319be1bf8eed6
VladislavSklv <79214579055@yandex.ru>
2022-01-29 00:49:14 +0300

    increase speed

--- dist/src/js/script.js
+++ dist/src/js/script.js
@@ -1,21 +1,3 @@
-/* $(document).ready(function(){
-
-$('.carousel__inner').slick({
-    speed: 500,
-    adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-arrow.png"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/right-arrow.png"></button>',
-    responsive: [
-        {
-            breakpoint: 992,
-            settings: {
-                arrows: false,
-            }
-        },
-    ]
-    });
-}); */
-
 const slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -55,13 +37,6 @@ $(document).ready(function(){
 
     //modals
 
-    /* function modalShow (tag, name){
-        $(tag).on('click', function() {
-            $(name).fadeIn();
-        });
-    }
-
-    modalShow('[data-modal=consultation]', '.overlay, #consultation') */
     $('[data-modal=consultation]').on('click', function() {
         $('.overlay, #consultation').fadeIn();
     });
